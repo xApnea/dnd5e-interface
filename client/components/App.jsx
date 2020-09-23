b73d7d3c9e162c2dd0dad5ec9c358c4bf1fea852
xApnea <jcbruno150@gmail.com>
2020-09-23 15:29:53 -0400

    Initialize react client

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>GIF Generator</h1>
+      </div>
+    )
+  }
+}
+
+export default App;

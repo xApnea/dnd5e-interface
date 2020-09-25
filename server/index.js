9a3e66f137345ec6effdae46f2a079a5b477a4e6
xApnea <jcbruno150@gmail.com>
2020-09-25 14:53:09 -0400

    Basic user registering routed

--- server/index.js
+++ server/index.js
@@ -17,6 +17,15 @@ app.get('/', (req, res) => {
   res.status(200).send('Request was good');
 })
 
+app.post('/users', (req, res) => {
+  let data = req.body;
+  if (!data.email || !data.username || !data.password || !data.confirmPassword) {
+    res.status(400).send('Please fill all of the fields to register');
+  } else {
+    res.status(200).send(`User: \"${data.username}\" registered with email: \"${data.email}\"`);
+  }
+})
+
 const port = process.env.PORT || 3000;
 var server = app.listen(port, () => {console.log(`Listening on port: ${port}`)});
 

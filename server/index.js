ea11c243c981152a86322d011ae847f6da7c6af3
xApnea <jcbruno150@gmail.com>
2020-09-25 14:44:10 -0400

    Added cors/json parser middleware, db connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
+const db = require('../db/index.js');
+const User = require('../db/model.js')
+require('dotenv').config();
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, '/../dist')));
 
 app.get('/', (req, res) => {

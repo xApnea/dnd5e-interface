d8ec06a4fe62a2d1eef15d429860bb6a5b686d7a
xApnea <jcbruno150@gmail.com>
2020-09-28 14:56:17 -0400

    Add auth middleware to verify jwtoken

---
+++ server/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const auth = function(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).json({ message: 'No authentication token.' });
+  }
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) {
+      return res.status(401).json({ message: 'Token is not valid, authorization denied.' });
+    }
+    req.user = verified.id;
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: 'Authorization failed.' });
+  }
+};
+
+module.exports = auth;

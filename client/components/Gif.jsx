14c5bc77a98950f2f44a2991708dafd5fc7b37ad
xApnea <jcbruno150@gmail.com>
2020-10-01 16:46:46 -0400

    Abstracted simple gif request to it's own component

---
+++ client/components/Gif.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+
+class Gif extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      gif: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/gifs/trending')
+      .then((res) => {
+        console.log(res);
+        const data = res.data;
+        this.setState({
+          gif : data
+        })
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <iframe src={this.state.gif.embed_url} width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href={this.state.gif.url}>via GIPHY</a></p>
+      </div>
+    )
+  }
+}
+
+export default Gif;
+

ec5f168e068c7c410609656b306c2b42e1894c21
xApnea <jcbruno150@gmail.com>
2020-10-01 16:49:00 -0400

    Abstracted Header and Home to their own components

---
+++ client/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <div>
+      <h3>GIF Generator</h3>
+      <button>Sign Up</button>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header;
---
+++ client/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+  return (
+    <div>
+      <h3>The Home Page</h3>
+    </div>
+  )
+}
+
+export default Home;

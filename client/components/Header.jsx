39ddba256e2088aac139a84fe9763fbf06733ab0
xApnea <jcbruno150@gmail.com>
2020-10-02 15:23:59 -0400

    Added routing buttons: Home/Title, Register, and Login switch to their respective components

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Header(props) {
+  const history = useHistory();
+
+  const home = () => history.push('/');
+  const register = () => history.push('/register');
+  const login = () => history.push('/login');
+
   return (
-    <div>
-      <h3>GIF Generator</h3>
-      <button>Sign Up</button>
-      <button>Login</button>
+    <div className='header'>
+      <h3 onClick={home}>GIF Generator</h3>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
     </div>
   )
 }

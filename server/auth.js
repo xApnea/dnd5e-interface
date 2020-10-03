7779d1ec53cb3b4becd964d7e9d2f464d54bff54
xApnea <jcbruno150@gmail.com>
2020-10-03 15:25:36 -0400

    Style consistency fix

--- server/auth.js
+++ server/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const auth = function(req, res, next) {
-  const token = req.header("x-auth-token");
+  const token = req.header('x-auth-token');
   if (!token) {
     return res.status(401).json({ message: 'No authentication token.' });
   }

ecec2c9e46b0afb4f06902158d6d266311dcab98
xApnea <jcbruno150@gmail.com>
2020-10-02 23:14:03 -0400

    Set up useEffect hook (executes on App mount) to check if a user was logged in before, and set user data state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
@@ -6,17 +6,45 @@ import Header from './Header.jsx';
 import Home from './Home.jsx';
 import Login from './Login.jsx';
 import Register from './Register.jsx';
+import UserContext from './context/Context.jsx';
 
-function App(props) {
+function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined
+  })
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem('auth-token');
+      if (token === null) {
+        localStorage.setItem('auth-token', '');
+        token = '';
+      }
+      let tokenResponse = await axios.post('/users/isTokenValid', null, {headers: {'x-auth-token' : token}});
+
+      let userData = tokenResponse.data ? await axios.get('/users');
+      setUserData({
+        token: userData.token,
+        user: userData.user
+      });
+    }
+
+    checkLoggedIn();
+
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/login' component={Login} />
-          <Route path='/register' component={Register} />
-        </Switch>
+        <UserContext.Provider value={{userData, setUserData}}>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+          </Switch>
+        </UserContext.Provider>
       </BrowserRouter>
     </div>
   )

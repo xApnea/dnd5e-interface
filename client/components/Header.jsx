927f4ab9561242d8bfcf5ce61500f75af65fac68
xApnea <jcbruno150@gmail.com>
2020-10-03 17:28:35 -0400

    Conditionally rendered nav buttons based on loggedin/loggedout state

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import UserContext from './context/Context.jsx';
 
 function Header(props) {
+  const { userData, setUserData } = useContext(UserContext);
   const history = useHistory();
 
   const home = () => history.push('/');
   const register = () => history.push('/register');
   const login = () => history.push('/login');
+  const logout = () => history.push('/logout');
 
   return (
     <div className='header'>
       <h3 onClick={home}>GIF Generator</h3>
-      <button onClick={register}>Register</button>
-      <button onClick={login}>Login</button>
+      <nav>
+        {
+          userData.user
+          ? <button>Log Out</button>
+          : <>
+              <button onClick={register}>Register</button>
+              <button onClick={login}>Log In</button>
+            </>
+        }
+      </nav>
     </div>
   )
 }

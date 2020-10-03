51eeda1e0b2e898a2b8759016cd88c7fc627d155
xApnea <jcbruno150@gmail.com>
2020-10-03 18:40:41 -0400

    Login form state is handled in local state class component

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 
-function Login(props) {
-  return (
-    <div>
-      <h3>The Login Page</h3>
-    </div>
-  )
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    }
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({email : event.target.value})
+  }
+
+  handlePasswordChange(event) {
+    this.setState({password : event.target.value})
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Login</h3>
+        <form onSubmit={this.handleSubmit}>
+
+          <label htmlFor='login-email'>Email:
+            <input id='login-email' type='email' autoComplete='email' value={this.state.email} onChange={this.handleEmailChange} />
+          </label>
+
+          <label htmlFor='login-password'>Password:
+            <input id='login-password' type='password' autoComplete='current-password' value={this.state.password} onChange={this.handlePasswordChange} />
+          </label>
+
+          <input type='submit' value='Login'/>
+
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Login;

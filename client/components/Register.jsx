81de70347c1ca1430b54219997524d77a1b85ece
xApnea <jcbruno150@gmail.com>
2020-10-04 14:16:48 -0400

    Finished refactor from class -> useState hook, added auto-login after register, redirect to home

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import Axios from 'axios';
+import UserContext from './context/Context.jsx';
 
-function Register {
+function Register() {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [role, setRole] = useState('user');
 
+  const { setUserData } = useContext(UserContext);
   const history = useHistory();
 
   const handleEmailChange = (event) => {
@@ -37,48 +39,55 @@ function Register {
         confirmPassword: confirmPassword,
         role: role
       })
-      alert(`User: ${result.data.user.username} successfully registered! Please log in to your new account.`)
-
-      //useHistory to redirect to login
+      if (result.data.user) {
+        result = await Axios.post('users/login', {
+          email: email,
+          password: password
+        })
+        setUserData({
+          token: result.data.token,
+          user: result.data.user
+        })
+        localStorage.setItem('auth-token', result.data.token);
+        history.push('/');
+      }
     }
     catch(error) {
       console.log(error);
     }
   }
 
-  render() {
-    return (
-      <div>
-        <h3>Register</h3>
-        <form onSubmit={handleSubmit}>
+  return (
+    <div>
+      <h3>Register</h3>
+      <form onSubmit={handleSubmit}>
 
-          <label htmlFor='register-email'>Email:
-            <input id='register-email' type='email' autoComplete='email' value={email} onChange={handleEmailChange} />
-          </label>
+        <label htmlFor='register-email'>Email:
+          <input id='register-email' type='email' autoComplete='email' value={email} onChange={handleEmailChange} />
+        </label>
 
-          <label htmlFor='register-username'>Username:
-            <input id='register-username' type='text' autoComplete='username' value={username} onChange={handleUsernameChange} />
-          </label>
+        <label htmlFor='register-username'>Username:
+          <input id='register-username' type='text' autoComplete='username' value={username} onChange={handleUsernameChange} />
+        </label>
 
-          <label htmlFor='register-password'>Password:
-            <input id='register-password' type='password' autoComplete='new-password' value={password} onChange={handlePasswordChange} />
-            <input id='register-confirmPassword' type='password' autoComplete='new-password' placeholder='Confirm Password' value={confirmPassword} onChange={handleConfirmPasswordChange} />
-          </label>
+        <label htmlFor='register-password'>Password:
+          <input id='register-password' type='password' autoComplete='new-password' value={password} onChange={handlePasswordChange} />
+          <input id='register-confirmPassword' type='password' autoComplete='new-password' placeholder='Confirm Password' value={confirmPassword} onChange={handleConfirmPasswordChange} />
+        </label>
 
-          <label htmlFor='register-role'>Role:
-            <select id='register-role' value={role} defaultValue='user' onChange={handleRoleChange}>
-              <option value='user'>User</option>
-              <option value='powerUser'>Power User</option>
-              <option value='admin'>Admin</option>
-            </select>
-          </label>
+        <label htmlFor='register-role'>Role:
+          <select id='register-role' value={role} onChange={handleRoleChange}>
+            <option value='user'>User</option>
+            <option value='powerUser'>Power User</option>
+            <option value='admin'>Admin</option>
+          </select>
+        </label>
 
-          <input type='submit' value='Register'/>
+        <input type='submit' value='Register'/>
 
-        </form>
-      </div>
-    )
-  }
+      </form>
+    </div>
+  )
 }
 
 export default Register;

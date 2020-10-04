b9b5b65eb50610d7ea8d99b54caed4172f147787
xApnea <jcbruno150@gmail.com>
2020-10-04 17:02:16 -0400

    Added button to save a gif to the db

--- client/components/Gif.jsx
+++ client/components/Gif.jsx
@@ -1,14 +1,33 @@
 import React, { useState, useContext, useEffect } from 'react';
 import Axios from 'axios';
+import UserContext from './context/Context.jsx';
 
 function Gif() {
   const [gif, setGif] = useState({});
+  const { userData, setUserData } = useContext(UserContext);
 
   const getANewTrendingGif = async () => {
     try {
       let result = await Axios.get('/gifs/trending');
       const data = result.data;
       setGif(data);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  const saveGif = async () => {
+    console.log(userData);
+    try {
+      let result = await Axios.patch('/gifs/save', {
+        id: userData.user.id,
+        gif: {
+          url: gif.url,
+          embed_url: gif.embed_url
+        }
+      });
+      console.log(result);
+      // Set userData to saved gif
     } catch(err) {
       console.error(err);
     }
@@ -22,6 +41,7 @@ function Gif() {
   return (
     <div>
       <button onClick={getANewTrendingGif}>Get New</button>
+      <button onClick={saveGif}>Save Gif</button>
       <div>
         <iframe src={gif.embed_url} width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
         <p><a href={gif.url}>via GIPHY</a></p>

9abd570c7dab05038900c06283463b41e586379e
xApnea <jcbruno150@gmail.com>
2020-10-19 14:23:26 -0400

    Linted for syntax and style consistency

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,25 +1,26 @@
+/* eslint-disable no-underscore-dangle */
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const User = require('./../../db/model.js');
+const User = require('../../db/model.js');
 const auth = require('../auth.js');
 
 router.post('/register', async (req, res) => {
-  let data = req.body;
+  const data = req.body;
 
   // Validate user information
   if (!data.email || !data.username || !data.password || !data.confirmPassword) {
-    res.status(400).json({message: 'Please fill all of the fields to register.'});
+    res.status(400).json({ message: 'Please fill all of the fields to register.' });
   }
   if (data.password.length < 6) {
-    res.status(400).json({message: 'Password must be at least 6 characters long.'});
+    res.status(400).json({ message: 'Password must be at least 6 characters long.' });
   }
   if (data.password !== data.confirmPassword) {
-    res.status(400).json({message: 'Passwords do not match.'});
+    res.status(400).json({ message: 'Passwords do not match.' });
   }
 
-  //if no role is passed in, default to basic user permission
+  // if no role is passed in, default to basic user permission
   if (!data.role) {
     data.role = 'user';
   }
@@ -33,69 +34,69 @@ router.post('/register', async (req, res) => {
     username: data.username,
     password: hash,
     role: data.role,
-    gif: {}
-  }
+    gif: {},
+  };
 
   // try to create a new user with the validate information
   User.create(newUser)
-  .then((user) => {
-    res.status(200).json({
-      message: 'User successfully registered.',
-      user: {
-        id: user._id,
-        email: user.email,
-        username: user.username,
-        role: user.role,
-        gif: user.gif
-      }
+    .then((user) => {
+      res.status(200).json({
+        message: 'User successfully registered.',
+        user: {
+          id: user._id,
+          email: user.email,
+          username: user.username,
+          role: user.role,
+          gif: user.gif,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      // determine whether the email or username is already taken
+      const duplicatedValue = Object.keys(err.keyValue);
+      res.status(500).json({ message: `A user already exists with that ${duplicatedValue[0]}` });
     });
-  })
-  .catch((err) => {
-    console.log(err);
-    // determine whether the email or username is already taken
-    let duplicatedValue = Object.keys(err.keyValue);
-    res.status(500).json({message: `A user already exists with that ${duplicatedValue[0]}`});
-  })
-})
+});
 
 router.post('/login', async (req, res) => {
   const data = req.body;
 
-  if (!data.email|| !data.password) {
-    res.status(400).json({message: 'Please enter your email and password to login.'});
+  if (!data.email || !data.password) {
+    res.status(400).json({ message: 'Please enter your email and password to login.' });
   }
 
-  User.findOne({email: data.email})
+  User.findOne({ email: data.email })
     .then((user) => {
       // compare hashed password
       bcrypt.compare(data.password, user.password)
         .then((isCorrect) => {
           if (!isCorrect) {
-            res.status(400).json({message: 'Password is invalid.'});
+            res.status(400).json({ message: 'Password is invalid.' });
           } else {
-            const token = jwt.sign({id : user._id}, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
             res.status(200).json({
-              token: token,
+              token,
               user: {
                 id: user._id,
                 username: user.username,
                 email: user.email,
                 role: user.role,
-                gif: user.gif
-              }
+                gif: user.gif,
+              },
             });
           }
-        })
+        });
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({message: 'An account with that email does not exist.'});
-    })
-})
+      res.status(500).json({ message: 'An account with that email does not exist.' });
+    });
+});
 
 router.delete('/delete', auth, async (req, res) => {
   // console.log(req.user);
-  User.findByIdAndDelete({_id: req.user})
+  User.findByIdAndDelete({ _id: req.user })
     .then((deletedUser) => {
       res.status(200).json({
         message: 'Successfully deleted user.',
@@ -103,15 +104,15 @@ router.delete('/delete', auth, async (req, res) => {
           id: deletedUser._id,
           username: deletedUser.username,
           email: deletedUser.email,
-          role: deletedUser.role
-        }
+          role: deletedUser.role,
+        },
       });
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).json({message: 'Deletion failed', error: err.message});
-    })
-})
+      res.status(500).json({ message: 'Deletion failed', error: err.message });
+    });
+});
 
 router.post('/isTokenValid', async (req, res) => {
   try {
@@ -125,15 +126,14 @@ router.post('/isTokenValid', async (req, res) => {
 
     // Ensure that that user still exists
     const user = await User.findById(verified.id);
-    if (!user) return res.status(500).json({message: 'User does not exist.'});
+    if (!user) return res.status(500).json({ message: 'User does not exist.' });
 
     // Send true if passes all validation
     res.status(200).json(true);
-
   } catch (err) {
-    res.status(500).json({error: err.message});
+    res.status(500).json({ error: err.message });
   }
-})
+});
 
 router.get('/', auth, (req, res) => {
   User.findById(req.user)
@@ -145,17 +145,17 @@ router.get('/', auth, (req, res) => {
           username: user.username,
           email: user.email,
           role: user.role,
-          gif: user.gif
-        }
+          gif: user.gif,
+        },
       });
     })
     .catch((err) => {
       console.error(err);
       res.status(500).send({
         message: 'Could not find user',
-        error: err.message
+        error: err.message,
       });
-    })
+    });
 });
 
 module.exports = router;
